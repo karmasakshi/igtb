efcc3d5de390831059e88d8be3348f7567917b2d
karmasakshi <karmasakshi121@gmail.com>
2016-10-21 12:11:05 +0530

    Add Summary Accounts template.

--- src/app/summary-accounts/summary-accounts.component.ts
+++ src/app/summary-accounts/summary-accounts.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-summary-accounts',
-  templateUrl: './summary-accounts.component.html',
-  styleUrls: ['./summary-accounts.component.css']
+    selector: 'app-summary-accounts',
+    templateUrl: './summary-accounts.component.html',
+    styleUrls: ['./summary-accounts.component.css']
 })
 export class SummaryAccountsComponent implements OnInit {
 
-  constructor() { }
+    accounts = [
+        {
+            currency: 'USD',
+            lastActivityTimestamp: '2016-10-20T08:28:57.480Z',
+            runningBalance: 123123,
+            availableBalance: 123123,
+            name: 'General New York Account'
+        },
+        {
+            currency: 'USD',
+            lastActivityTimestamp: '2016-10-20T08:28:57.480Z',
+            runningBalance: 123123,
+            availableBalance: 123123,
+            name: 'Secondary New York Account'
+        },
+        {
+            currency: 'USD',
+            name: 'General Salary Account',
+            lastActivityTimestamp: '2016-10-20T08:28:57.480Z',
+            runningBalance: 123123,
+            availableBalance: 123123
+        }
+    ];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
