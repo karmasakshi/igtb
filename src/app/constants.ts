b144857c8b004124b2800575b7004df0f1b35443
karmasakshi <karmasakshi121@gmail.com>
2016-11-14 12:11:19 +0530

    Typo.

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 export const ENV = 'PROD';
 export const VERSION = 1;
 export const ENDPOINTS = {
-    "GET_SUMMARY": 'https://robin-daniel-consultants.cloud.tyk.io/accounts/summary';
+    "GET_SUMMARY": 'https://robin-daniel-consultants.cloud.tyk.io/accounts/summary'
 };
 export const TIMEOUT = 3000;
